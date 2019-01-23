cdca7b40d61bb48a73bd7d6961b7d3748606e537
TheChemistMD <andriyyatsykiv@gmail.com>
2019-01-23 00:23:57 -0500

    Revert "Circumference T Points"
    This reverts commit 4a61cce6ab48f5970d9de88a2401a92861b996de.

--- EEGcalc.js
+++ EEGcalc.js
@@ -4,22 +4,27 @@
 //Calculate Distances
 function calculateDistances() {
   var NasInVar = document.getElementById("NasInDist").value;
+  var EarEarVar = document.getElementById("EarEarDist").value;
+  var HeadCircVar = document.getElementById("HeadCirc").value;
 
 	
 
   //Calculate distances
-  var FzpVar = (NasInVar * 0.9);
+  var FzpVar = (NasInVar * 0.1);
   
-  var FzVar = (NasInVar * 0.7);
+  var FzVar = (NasInVar * 0.3);
   
-
+	//Though C4Var is calculated, it remains unused as it is equivalent to C3Var, and we can output one distance which covers both C3&C4 placement.
   var CzVar = (NasInVar * 0.5);
-
+  var C3Var = (EarEarVar * 0.2);
+  var C4Var = (EarEarVar * 0.2);
  
   var PzVar = (NasInVar * 0.3);
   
+	//Though both O1 and O2 distances calculated, only O1 distance used as they are symmetrical around Oz.
   var OzVar = (NasInVar * 0.1);
-
+  var O1Var = (HeadCircVar * 0.05);
+  var O2Var = (HeadCircVar * 0.05);
   
   //round to two decimal places, prevents excessively long calculator outputs
   FzpVar = Math.round(FzpVar * 100) / 100;
@@ -27,7 +32,6 @@ function calculateDistances() {
   CzVar = Math.round(CzVar * 100) / 100;
   PzVar = Math.round(PzVar * 100) / 100;
   OzVar = Math.round(OzVar * 100) / 100;
-
   
   
   //next line allows us to always have two digits after decimal point
@@ -36,28 +40,27 @@ function calculateDistances() {
   CzVar = CzVar.toFixed(2);
   PzVar = PzVar.toFixed(2);
   OzVar = OzVar.toFixed(2);
-
+  
   
   //Display the calculated distances
 
-
+  
   document.getElementById("FzpElement").innerHTML = FzpVar;
   
   document.getElementById("FzElement").innerHTML = FzVar;
   
-   document.getElementById("CzElement").innerHTML = CzVar;
-
+  //Use only C3Var to output both C3 and C4 values because they are symmetric around Cz
+  document.getElementById("CzElement").innerHTML = CzVar;
+  document.getElementById("C3Element").innerHTML = C3Var;
   
   document.getElementById("PzElement").innerHTML = PzVar;
   
   document.getElementById("OzElement").innerHTML = OzVar;
-
+  document.getElementById("O1Element").innerHTML = O1Var;
   
 
 
 }
-
-
 
 //TO DO - Hide Distances before calculation.
 
@@ -65,47 +68,7 @@ function calculateDistances() {
 
 
 //click to call function
-document.getElementById("MedialButton").onclick = function() {
+document.getElementById("calculate").onclick = function() {
   calculateDistances();
-
-};
-
-
-//--------------------------------------------------------------
-
-
-//Next, we calculate the rest of the points
-function CalculateFromTpointsF7F8() {
-	var FpzOzCircVar = document.getElementById("FpzOzCirc").value;
-
-	
-	  //Calculate distances
-  var O1distvar = (FpzOzCircVar * 0.05);
-  var T5distvar = (FpzOzCircVar * 0.15);
-  var T3distvar = (FpzOzCircVar * 0.25);
-  var F7distvar = (FpzOzCircVar * 0.35);
-  var F8distvar = (FpzOzCircVar * 0.65);
-  var T4distvar = (FpzOzCircVar * 0.75);
-  var T6distvar = (FpzOzCircVar * 0.85);
-  var O2distvar = (FpzOzCircVar * 0.95);
-
-  
-  //Display Distances
-  document.getElementById("O1Element").innerHTML = O1distvar;
-  document.getElementById("T5Element").innerHTML = T5distvar;
-  document.getElementById("T3Element").innerHTML = T3distvar;
-  document.getElementById("F7Element").innerHTML = F7distvar;
-  document.getElementById("F8Element").innerHTML = F8distvar;
-  document.getElementById("T4Element").innerHTML = T4distvar;
-  document.getElementById("T6Element").innerHTML = T6distvar;
-  document.getElementById("O2Element").innerHTML = O2distvar;
-  
-  
-  // End of Function
-}
-
-//click to call Circumference Function
-document.getElementById("CircumferenceButton").onclick = function() {
-  CalculateFromTpointsF7F8();
 
 };
